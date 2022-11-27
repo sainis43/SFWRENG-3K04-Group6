526298e4bde91c9c9c5a1f8d4dfe0d3dde353ce5
Sohel Saini <89737622+sainis43@users.noreply.github.com>
2022-11-27 11:42:44 -0500

    Edited the Serial function file so signal set can work in DCM File

--- DCM/serialcom.py
+++ DCM/serialcom.py
@@ -82,26 +82,26 @@ else:
     RF_en = struct.pack(">B", int(RF))
     RyT_en = struct.pack(">B", int(RyT))
 
-print("Start: ", Start)
-print("SYNC: ", SYNC)
-print("Fn_set: ", Fn_set)
-print("mode: ", mode_en)
-print("LRL_en: ", LRL_en)
-print("URL_en: ", URL_en)
-print("MSR_en: ", MSR_en)
-print("AA_en: ", AA_en)
-print("APW_en: ", APW_en)
-print("AS_en: ", AS_en)
-print("VA_en: ", VA_en)
-print("VPW_en: ", VPW_en)
-print("VS_en: ", VS_en)
-print("ARP_en: ", ARP_en)
-print("VRP_en: ", VRP_en)
-print("PVARP_en: ", PVARP_en)
-print("AT_en: ", AT_en)
-print("RnT_en: ", RnT_en)
-print("RF_en: ", RF_en)
-print("RyT_en: ", RyT_en)
+##print("Start: ", Start)
+##print("SYNC: ", SYNC)
+##print("Fn_set: ", Fn_set)
+##print("mode: ", mode_en)
+##print("LRL_en: ", LRL_en)
+##print("URL_en: ", URL_en)
+##print("MSR_en: ", MSR_en)
+##print("AA_en: ", AA_en)
+##print("APW_en: ", APW_en)
+##print("AS_en: ", AS_en)
+##print("VA_en: ", VA_en)
+##print("VPW_en: ", VPW_en)
+##print("VS_en: ", VS_en)
+##print("ARP_en: ", ARP_en)
+##print("VRP_en: ", VRP_en)
+##print("PVARP_en: ", PVARP_en)
+##print("AT_en: ", AT_en)
+##print("RnT_en: ", RnT_en)
+##print("RF_en: ", RF_en)
+##print("RyT_en: ", RyT_en)
 
 mode_de = struct.unpack("B", mode_en)
 LRL_de = struct.unpack("B", LRL_en)
@@ -121,21 +121,62 @@ RnT_de = struct.unpack("B", RnT_en)
 RF_de = struct.unpack("B", RF_en)
 RyT_de = struct.unpack("B", RyT_en)
 
-print("mode_en: ", mode_de[0])
-print("LRL_en: ", LRL_de[0])
-print("URL_en: ", URL_de[0])
-print("MSR_en: ", MSR_de[0])
-print("AA_en: ", AA_de[0])
-print("APW_en: ", APW_de[0])
-print("AS_en: ", AS_de[0])
-print("VA_en: ", VA_de[0])
-print("VPW_en: ", VPW_de[0])
-print("VS_en: ", VS_de[0])
-print("ARP_en: ", ARP_de[0])
-print("VRP_en: ", VRP_de[0])
-print("PVARP_en: ", PVARP_de[0])
-print("AT_en: ", AT_de[0])
-print("RnT_en: ", RnT_de[0])
-print("RF_en: ", RF_de[0])
-print("RyT_en: ", RyT_de[0])
 
+##print("mode_en: ", mode_de[0])
+##print("LRL_en: ", LRL_de[0])
+##print("URL_en: ", URL_de[0])
+##print("MSR_en: ", MSR_de[0])
+##print("AA_en: ", AA_de[0])
+##print("APW_en: ", APW_de[0])
+##print("AS_en: ", AS_de[0])
+##print("VA_en: ", VA_de[0])
+##print("VPW_en: ", VPW_de[0])
+##print("VS_en: ", VS_de[0])
+##print("ARP_en: ", ARP_de[0])
+##print("VRP_en: ", VRP_de[0])
+##print("PVARP_en: ", PVARP_de[0])
+##print("AT_en: ", AT_de[0])
+##print("RnT_en: ", RnT_de[0])
+##print("RF_en: ", RF_de[0])
+##print("RyT_en: ", RyT_de[0])
+
+
+
+
+Signal_set = Start + Fn_set + mode_en + LRL_en + URL_en + AA_en + APW_en + VA_en + VPW_en + VRP_en + ARP_en + MSR_en + AS_en + VS_en + PVARP_en + AT_en + RnT_en + RF_en + RyT_en
+print(Signal_set)
+
+#Signal_echo = Start + SYNC + mode_en + LRL_en + URL_en + AA_en + APW_en + VA_en + VPW_en + VRP_en + ARP_en + MSR_en + AS_en + VS_en + PVARP_en + AT_en + RnT_en + RF_en + RyT_en
+
+#with serial.Serial(frdm_port, 115200) as pacemaker:
+#    pacemaker.write(Signal_set)
+
+##with serial.Serial(frdm_port, 115200) as pacemaker:
+##    pacemaker.write(Signal_echo)
+##    data = pacemaker.read(9)
+##    mode = data[0]
+##    LRL = data[1]
+##    URL =data[2]
+##    AA = data[3]
+##    APW = data[4]
+##    VA = data[5]
+##    VPW = data[6]
+##    VRP = data[7]
+##    ARP = data[8]
+##    MSR = data[9]
+##    AS = data[10]
+##    VS = data[11]
+##    PVARP = data[12]
+##    AT = data[13]
+##    RnT = data[14]
+##    RF = data[15]
+##    RyT = data[16]
+##    off_rev =  struct.unpack("f", data[3:7])[0]
+##    switch_rev =  struct.unpack("H", data[7:9])[0]
+
+##print("From the board:")
+##print("red_en = ", red_rev)
+##print("green_en = ", green_rev)
+##print("blue_en = ", blue_rev)
+##print("off_time = ",  off_rev)
+##print("switch_time = ", switch_rev)
